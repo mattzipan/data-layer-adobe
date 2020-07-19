44b44445a721f5aaeeecce74d674dc7b4592158a
mattzipan <mattzipan@gmail.com>
2020-07-19 12:39:39 +0200

    first commit

---
+++ data-layer-initialize.js
@@ -0,0 +1,7 @@
+window.digitalData = {
+  pageName: "sneaker overview",
+  destinationPath: "/en/sneakers",
+  breadCrumbs: ["home","sneakers"],
+  publishDate: "2020-07-01",
+  language: "en-US"
+};
---
+++ track-interaction.js
@@ -0,0 +1,3 @@
+//Event with chosen color
+_satellite.setVar("sneaker color", "black");
+_satellite.track("select color"); 
